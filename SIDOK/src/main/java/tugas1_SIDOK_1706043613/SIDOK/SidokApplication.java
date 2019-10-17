f44173e293a2bc337174d89d230fe4b25cf6f293
anhilym <hilmyahmadnaufal@gmail.com>
2019-10-17 10:31:59 +0700

    feat: init SIDOK

---
+++ SIDOK/src/main/java/tugas1_SIDOK_1706043613/SIDOK/SidokApplication.java
@@ -0,0 +1,13 @@
+package tugas1_SIDOK_1706043613.SIDOK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidokApplication.class, args);
+	}
+
+}
---
+++ SIDOK/src/test/java/tugas1_SIDOK_1706043613/SIDOK/SidokApplicationTests.java
@@ -0,0 +1,13 @@
+package tugas1_SIDOK_1706043613.SIDOK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
